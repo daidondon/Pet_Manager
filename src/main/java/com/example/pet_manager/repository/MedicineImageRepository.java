9a1804d5e75eda45f7b6083edc3de627ff922c75
daidondon <dai2452001@gmail.com>
2024-06-22 02:06:08 +0700

    a

--- src/main/java/com/example/pet_manager/repository/MedicineImageRepository.java
+++ src/main/java/com/example/pet_manager/repository/MedicineImageRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface MedicineImageRepository extends JpaRepository<MedicineImageRepository, Integer> {
+public interface MedicineImageRepository extends JpaRepository<MedicineImage, Integer> {
 }
