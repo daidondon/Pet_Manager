8c06c5050de607debf22f27f67c2d68b1cc950b0
chiennmhe153274 <chiennmhe153174@fpt.edu.vn>
2024-06-27 10:52:08 +0700

    add repo authen author

---
+++ repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.petbe.repositories;
+
+import com.example.petbe.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(
+            "SELECT u FROM User u WHERE u.gmail = :gmail"
+    )
+    Optional<User> findByGmail(@Param("gmail") String gmail);
+
+    Boolean existsByGmail(String gmail);
+}
