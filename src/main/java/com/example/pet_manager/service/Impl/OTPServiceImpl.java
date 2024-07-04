333319ee3e3d89a8fd3abc45fa6b5b82329cf71a
chiennmhe153274 <chiennmhe153174@fpt.edu.vn>
2024-07-04 12:13:17 +0700

    otp service

---
+++ src/main/java/com/example/pet_manager/service/Impl/OTPServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.pet_manager.service.Impl;
+
+import com.example.pet_manager.service.EmailService;
+import com.twilio.http.TwilioRestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class OTPServiceImpl {
+    @Value("${twilio.phoneNumber}")
+    private String twilioPhoneNumber;
+
+
+
+    private final TwilioRestClient twilioRestClient;
+
+    public OTPServiceImpl(@Value("${twilio.accountSid}") String accountSid,
+                      @Value("${twilio.authToken}") String authToken) {
+        twilioRestClient = new TwilioRestClient.Builder(accountSid, authToken).build();
+    }
+
+
+    public void sendOtp(String recipientPhoneNumber, String otpCode) {
+        Message.creator(new PhoneNumber(recipientPhoneNumber), new PhoneNumber(twilioPhoneNumber),
+                        "Your OTP code is: " + otpCode)
+                .create(twilioRestClient);
+    }
+}
